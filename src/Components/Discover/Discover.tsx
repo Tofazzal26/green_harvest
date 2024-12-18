124e5387613426138c5a61e70dd5843091d8342e
Tofazzal26 <afranislamabir6789@gmail.com>
2024-12-18 09:40:15 +0600

    discover style finish

---
+++ src/Components/Discover/Discover.tsx
@@ -0,0 +1,27 @@
+import { ArrowUpRight } from "lucide-react";
+
+const Discover = () => {
+  return (
+    <div className="my-10">
+      <div className="relative md:w-full md:h-[270px] h-[400px] bg-cover bg-no-repeat bg-center bg-[url('/rice.jpg')] rounded-[30px]">
+        <div className="bg-gray-900 absolute inset-0 rounded-[30px] bg-opacity-40">
+          <div className="flex md:flex-row flex-col justify-between md:px-[80px] items-center md:translate-y-[60px]">
+            <div className="flex md:flex-row flex-col items-center gap-2">
+              <img src="/tree.png" alt="" />
+              <h2 className="text-[30px] text-center md:text-start md:text-[50px] md:w-[600px] text-white md:leading-[60px]">
+                We’re popular leader in agriculture market globally
+              </h2>
+            </div>
+            <div>
+              <button className="bg-white rounded-full px-8 gap-2 py-3 text-gray-600 flex items-center text-[20px]">
+                Discover More <ArrowUpRight size={24} />{" "}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "@/Components/Banner/Banner";
 import BrandSlider from "@/Components/BrandSlider/BrandSlider";
 import ClientBrand from "@/Components/ClientBrand/ClientBrand";
+import Discover from "@/Components/Discover/Discover";
 import GrowthHarvest from "@/Components/GrowthHarvest/GrowthHarvest";
 import NewsInsight from "@/Components/NewsInsight/NewsInsight";
 import OurBenefits from "@/Components/OurBenefits/OurBenefits";
@@ -15,6 +16,7 @@ export default function Home() {
       <OurBenefits />
       <NewsInsight />
       <ClientBrand />
+      <Discover />
     </div>
   );
 }
